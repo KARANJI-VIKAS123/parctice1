5c5b9304b764fd1780503650460667653b791cad
karanjivikas <karanjivikas03@gmail.com>
2026-01-28 17:32:17 +0530

    prime number or not added

---
+++ prime number or not.py
@@ -0,0 +1,12 @@
+n=int(input("enter a number:"))
+count=0
+for i in range(n):
+    if n%2==0:
+        
+        count=count+1
+    
+if count==2:
+    print("prime number")
+else:
+    print("not a prime number:")
+    
