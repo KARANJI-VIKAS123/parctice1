c6b15393dc99d6d2fedc65033a0ecb83d66e61fc
Karanji Vikas <karanjivikas03@gmail.com>
2026-01-29 16:46:41 +0530

    Fix reverse function call in Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -10,7 +10,7 @@ def reverse(num):
 def ispalindrome(num):
     return num == reverse(num)
 
-print(reverse(123))        # 321
+print(reverse(123456))        # 321
 print(ispalindrome(123))  # False
 print(reverse(121))        # 121
 print(ispalindrome(121))  # True
